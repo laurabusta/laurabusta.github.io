9621315ea3528ab2a6a33cc4fa29c1d540529852
Laura Bustamante <laurabustamante@Lauras-2017-MacBook-Pro.local>
2021-02-08 17:19:23 -0500

    completed code to load data into a master array of objects

--- jeopardy-game/js/app.js
+++ jeopardy-game/js/app.js
@@ -8,22 +8,31 @@ console.log("hello world");
 
 // create an array of answer objects
 
-class answer {
-    constructor () {
+class Answer {
+    constructor (category, dollar_value, answer, choicesArray, solution) {
         this.category = category;
         this.dollar_value = dollar_value;
         this.answer = answer;
-        this.choices = [];
-        this.solution = null;
+        this.choicesArray = choicesArray;
+        this.solution = solution;
     }
 }
 
-// load data to answer array
-// 
+////////////////////////////////////////////////
+// data to load into array of answer objects
+////////////////////////////////////////////////
 
 const categoryArray = ['CATEGORY ONE', 'CATEGORY TWO', 'CATEGORY THREE', 'CATEGORY FOUR', 'CATEGORY FIVE', 'CATEGORY SIX'];
 const dollarValueArray = ['$100', '$200', '$300', '$400', '$500'];
 const categoryAnswerArray = [
+    ['cat1:answer $100', 'cat1:answer $200', 'cat1:answer $300', 'cat1:answer $400', 'cat1:answer $500'],
+    ['cat2:answer $100', 'cat2:answer $200', 'cat2:answer $300', 'cat2:answer $400', 'cat2:answer $500'],
+    ['cat3:answer $100', 'cat3:answer $200', 'cat3:answer $300', 'cat3:answer $400', 'cat3:answer $500'],
+    ['cat4:answer $100', 'cat4:answer $200', 'cat4:answer $300', 'cat4:answer $400', 'cat4:answer $500'],
+    ['cat5:answer $100', 'cat5:answer $200', 'cat5:answer $300', 'cat5:answer $400', 'cat5:answer $500'],
+    ['cat6:answer $100', 'cat6:answer $200', 'cat6:answer $300', 'cat6:answer $400', 'cat6:answer $500']
+];
+const categoryChoicesArray = [
     [
         ['cat1:choice $100a', 'cat1:choice $100b', 'cat1:choice $100c', 'cat1:choice $100d'],
         ['cat1:choice $200a', 'cat1:choice $200b', 'cat1:choice $200c', 'cat1:choice $200d'],
@@ -73,11 +82,44 @@ const solutionKey = [
     [0,1,2,3,0],
     [0,1,2,3,0],
     [0,1,2,3,0],
+    [0,1,2,3,0]
 ];
-console.log(categoryArray[0]);
-console.log(dollarValueArray[1]);
-console.log(categoryAnswerArray[5][3]);
-console.log(solutionKey[1][3]);
+
+// tested the data arrays
+// console.log(categoryArray[0]);
+// console.log(dollarValueArray[1]);
+// console.log(categoryAnswerArray[2][3]);
+// console.log(categoryChoicesArray[5][3]);
+// console.log(solutionKey[1][3]);
+
+////////////////////////////////////////////////////
+// load data into array of objects of class answer
+////////////////////////////////////////////////////
+
+// test creating one Answer object
+// const testAnswerObject = new Answer(categoryArray[0], dollarValueArray[0], categoryAnswerArray[0][0], categoryChoicesArray[0][0], solutionKey[0][0]);
+// console.log(testAnswerObject);
+
+// category loop {
+//
+//      {answer loop
+//          create array of answer objects for category}
+//
+//      push array of answer objects into master array of categories
+//      gameAnswerArray [0][0] - 2d array, [categoryIndex], [dollarValueIndex]
+// }
+
+const masterAnswerArray = [];
+let tempAnswerObjectArray = [];
+for (let categoryIndex=0; categoryIndex<6; categoryIndex++) {
+    for (let answerIndex=0; answerIndex<5; answerIndex++) {
+        const tempAnswerObject = new Answer(categoryArray[categoryIndex], dollarValueArray[answerIndex], categoryAnswerArray[categoryIndex][answerIndex], categoryChoicesArray[categoryIndex][answerIndex], solutionKey[categoryIndex][answerIndex]);
+        tempAnswerObjectArray.push(tempAnswerObject);
+    };
+    masterAnswerArray.push(tempAnswerObjectArray);
+    tempAnswerObjectArray = [];  // clear the temporary array of Answers that are pushed on the master Array for next loop iteration
+};
+console.log(masterAnswerArray);
 
 ///////////////////////////////
 // event handling
@@ -89,6 +131,7 @@ $( () => {
         console.log('box clicked');
         console.log($(event.currentTarget).attr('id'));
         // identify which answer is chosen, call function that returns answer object
+        console.log(Number.isNaN($(event.currentTarget).attr('id'))); //checking if id is a number
     })
 
 });
